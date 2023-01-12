dd5c9a11999c6c622677ba428416249b98104e54
VgnTS-yw7J <119039190+VgnTS-yw7J@users.noreply.github.com>
2023-01-12 21:37:20 +0100

    Beispieldatei Rechner.py hinzugefügt

---
+++ Rechner.py
@@ -0,0 +1,13 @@
+from tkinter import *
+from tkinter import ttk
+
+
+root = Tk()
+root.title("Stromrechner v1")
+root.geometry("600x600")
+root.resizable(width=False, height=False)
+
+mainFrame = Label(root, text="Poggers")
+mainFrame.pack()
+
+root.mainloop()
