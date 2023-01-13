5e28c7b33b21b6f6505a9274767a9e526fee6305
VgnTS-yw7J <119039190+VgnTS-yw7J@users.noreply.github.com>
2023-01-13 11:22:33 +0100

    erster basic prototyp, anpassungen folgen

--- Rechner.py
+++ Rechner.py
@@ -1,13 +1,35 @@
 from tkinter import *
-from tkinter import ttk
-
 
 root = Tk()
-root.title("Stromrechner v1")
+root.title("Lustiger Titel")
 root.geometry("600x600")
-root.resizable(width=False, height=False)
 
-topLabel = Label(root, text="Poggers")
-topLabel.pack()
+label_watt = Label(root, text="Verbrauch in Watt")
+label_watt.grid()
+
+entry_watt = Entry(root)
+entry_watt.grid(column=2, row=0)
+
+label_betriebsstunden = Label(root, text="Betriebsstunden / Tag")
+label_betriebsstunden.grid(column=3, row=0)
+
+entry_betriebsstunden = Entry(root)
+entry_betriebsstunden.grid(column=4, row=0)
+
+label_result = Label(root, text="Watt per hours:")
+label_result.grid(column=0, row=1)
+
+label_result_2 = Label(root, text="")
+label_result_2.grid(column=1, row=1)
+
+
+def calculate():
+    label_result_2.configure(
+        text=f"{int(entry_watt.get())*int(entry_betriebsstunden.get())}"
+    )
+
+
+button_calculate = Button(root, text="Calculate Watt per hours", command=calculate)
+button_calculate.grid(column=0, row=2)
 
 root.mainloop()
