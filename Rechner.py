64830b34172ba59c12c6388e29c5b362e81b3a4a
VgnTS-yw7J <119039190+VgnTS-yw7J@users.noreply.github.com>
2023-01-13 09:28:26 +0100

    namen des labels angepasst

--- Rechner.py
+++ Rechner.py
@@ -7,7 +7,7 @@ root.title("Stromrechner v1")
 root.geometry("600x600")
 root.resizable(width=False, height=False)
 
-mainFrame = Label(root, text="Poggers")
-mainFrame.pack()
+topLabel = Label(root, text="Poggers")
+topLabel.pack()
 
 root.mainloop()
