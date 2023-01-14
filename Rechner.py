89d1fbdb2b2234ca9b6d6772828a5b0ed0a37d7f
VgnTS-yw7J <119039190+VgnTS-yw7J@users.noreply.github.com>
2023-01-14 18:03:51 +0100

    bessere verwertung des strompreises

--- Rechner.py
+++ Rechner.py
@@ -1,4 +1,5 @@
 import pip
+
 pip.main(["install", "ttkbootstrap"])
 import ttkbootstrap as ttk
 from ttkbootstrap.constants import *
@@ -32,11 +33,9 @@ entry_betriebsstunden.grid(column=3, row=0, sticky=NW, padx=(5, 0))
 label_betriebstage_week = ttk.Label(mainFrame, text="Betriebstage / Woche:")
 label_betriebstage_week.grid(column=4, row=0, sticky=NW, padx=(20, 0))
 
-number_of_days = [1, 2, 3, 4, 5, 6, 7]
+days = [1, 2, 3, 4, 5, 6, 7]
 
-combobox_betriebstage_week_2 = ttk.Combobox(
-    mainFrame, values=number_of_days, state="readonly"
-)
+combobox_betriebstage_week_2 = ttk.Combobox(mainFrame, values=days, state="readonly")
 combobox_betriebstage_week_2.current(0)
 combobox_betriebstage_week_2.grid(column=5, row=0, sticky=NW, padx=(5, 0))
 
@@ -44,62 +43,63 @@ combobox_betriebstage_week_2.grid(column=5, row=0, sticky=NW, padx=(5, 0))
 label_strompreis = ttk.Label(mainFrame, text="Strompreis pro kWh:")
 label_strompreis.grid(column=6, row=0, sticky=NW, padx=(20, 0))
 
-entry_strompreis =ttk.Entry(mainFrame)
+entry_strompreis = ttk.Entry(mainFrame)
 entry_strompreis.grid(column=7, row=0, sticky=NW, padx=(5, 0))
 
 # ANZEIGEN ------------------------------------------------------------------
 # anzeige kwh pro tag
-label_kwh_day =ttk.Label(mainFrame, text="Verbrauch pro Tag:")
+label_kwh_day = ttk.Label(mainFrame, text="Verbrauch pro Tag:")
 label_kwh_day.grid(column=0, row=1, sticky=W, pady=(10, 0))
 
-label_kwh_day_2 =ttk.Label(mainFrame, text="")
+label_kwh_day_2 = ttk.Label(mainFrame, text="")
 label_kwh_day_2.grid(column=1, row=1, sticky=W, padx=(4, 0))
 # anzeige kwh pro woche
-label_kwh_week =ttk.Label(mainFrame, text="Verbrauch pro Woche:")
+label_kwh_week = ttk.Label(mainFrame, text="Verbrauch pro Woche:")
 label_kwh_week.grid(column=0, row=2, sticky=W)
 
-label_kwh_week_2 =ttk.Label(mainFrame, text="")
+label_kwh_week_2 = ttk.Label(mainFrame, text="")
 label_kwh_week_2.grid(column=1, row=2, sticky=W, padx=(4, 0))
 # anzeige kwh pro monat
-label_kwh_month =ttk.Label(mainFrame, text="Verbrauch pro Monat:")
+label_kwh_month = ttk.Label(mainFrame, text="Verbrauch pro Monat:")
 label_kwh_month.grid(column=0, row=3, sticky=W)
 
-label_kwh_month_2 =ttk.Label(mainFrame, text="")
+label_kwh_month_2 = ttk.Label(mainFrame, text="")
 label_kwh_month_2.grid(column=1, row=3, sticky=W, padx=(4, 0))
 # anzeige kwh pro jahr
-label_kwh_year =ttk.Label(mainFrame, text="Verbrauch pro Jahr:")
+label_kwh_year = ttk.Label(mainFrame, text="Verbrauch pro Jahr:")
 label_kwh_year.grid(column=0, row=4, sticky=W)
 
-label_kwh_year_2 =ttk.Label(mainFrame, text="")
+label_kwh_year_2 = ttk.Label(mainFrame, text="")
 label_kwh_year_2.grid(column=1, row=4, sticky=W, padx=(4, 0))
 # KOSTEN
 # anzeige kosten pro tag
-label_kosten_day =ttk.Label(mainFrame, text="Kosten pro Tag:")
+label_kosten_day = ttk.Label(mainFrame, text="Kosten pro Tag:")
 label_kosten_day.grid(column=0, row=5, sticky=W, pady=(10, 0))
 
-label_kosten_day_2 =ttk.Label(mainFrame, text="")
+label_kosten_day_2 = ttk.Label(mainFrame, text="")
 label_kosten_day_2.grid(column=1, row=5, sticky=W, padx=(4, 0))
 # anzeige kosten pro woche
-label_kosten_week =ttk.Label(mainFrame, text="Kosten pro Woche:")
+label_kosten_week = ttk.Label(mainFrame, text="Kosten pro Woche:")
 label_kosten_week.grid(column=0, row=6, sticky=W)
 
-label_kosten_week_2 =ttk.Label(mainFrame, text="")
+label_kosten_week_2 = ttk.Label(mainFrame, text="")
 label_kosten_week_2.grid(column=1, row=6, sticky=W, padx=(4, 0))
 # anzeige kosten pro monat
-label_kosten_month =ttk.Label(mainFrame, text="Kosten pro Monat:")
+label_kosten_month = ttk.Label(mainFrame, text="Kosten pro Monat:")
 label_kosten_month.grid(column=0, row=7, sticky=W)
 
-label_kosten_month_2 =ttk.Label(mainFrame, text="")
+label_kosten_month_2 = ttk.Label(mainFrame, text="")
 label_kosten_month_2.grid(column=1, row=7, sticky=W, padx=(4, 0))
 # anzeige kosten pro jahr
-label_kosten_year =ttk.Label(mainFrame, text="Kosten pro Jahr:")
+label_kosten_year = ttk.Label(mainFrame, text="Kosten pro Jahr:")
 label_kosten_year.grid(column=0, row=8, sticky=W)
 
-label_kosten_year_2 =ttk.Label(mainFrame, text="")
+label_kosten_year_2 = ttk.Label(mainFrame, text="")
 label_kosten_year_2.grid(column=1, row=8, sticky=W, padx=(4, 0))
 
 # rechenoperation
 def calculate():
+    stromkosten = entry_strompreis.get().replace(".", "").replace(",", ".")
     try:
         label_kwh_day_2.configure(
             text=f"{int(entry_watt.get())*int(entry_betriebsstunden.get())/1000} kWh"
@@ -130,28 +130,28 @@ def calculate():
     # KOSTEN
     try:
         label_kosten_day_2.configure(
-            text=f"{int(entry_watt.get())*int(entry_betriebsstunden.get())/1000*float(entry_strompreis.get())} €"
+            text=f"{int(entry_watt.get())*int(entry_betriebsstunden.get())/1000*float(stromkosten)} €"
         )
     except:
         pass
 
     try:
         label_kosten_week_2.configure(
-            text=f"{int(entry_watt.get())*int(entry_betriebsstunden.get())*int(combobox_betriebstage_week_2.get())/1000*float(entry_strompreis.get())} €"
+            text=f"{int(entry_watt.get())*int(entry_betriebsstunden.get())*int(combobox_betriebstage_week_2.get())/1000*float(stromkosten)} €"
         )
     except:
         pass
 
     try:
         label_kosten_month_2.configure(
-            text=f"{int(entry_watt.get())*int(entry_betriebsstunden.get())*int(combobox_betriebstage_week_2.get())*4.3/1000*float(entry_strompreis.get())} €"
+            text=f"{int(entry_watt.get())*int(entry_betriebsstunden.get())*int(combobox_betriebstage_week_2.get())*4.3/1000*float(stromkosten)} €"
         )
     except:
         pass
 
     try:
         label_kosten_year_2.configure(
-            text=f"{int(entry_watt.get())*int(entry_betriebsstunden.get())*int(combobox_betriebstage_week_2.get())*52.1/1000*float(entry_strompreis.get())} €"
+            text=f"{int(entry_watt.get())*int(entry_betriebsstunden.get())*int(combobox_betriebstage_week_2.get())*52.1/1000*float(stromkosten)} €"
         )
     except:
         pass
