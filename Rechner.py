098412ab30b93c93a57098d70d00030d1a4c83c2
VgnTS-yw7J <119039190+VgnTS-yw7J@users.noreply.github.com>
2023-01-18 14:20:29 +0100

    switching workplace

--- Rechner.py
+++ Rechner.py
@@ -1,10 +1,12 @@
 from tkinter import *
 from tkinter import ttk
+from tkinter import tix
 
+items = []
 # root window
 root = Tk()
 root.title("Stromrechner v1")
-root.geometry("1280x720")
+root.geometry("1000x600")
 root.resizable(width=False, height=False)
 root.tk.call("source", "azure.tcl")
 root.tk.call("set_theme", "dark")
@@ -49,54 +51,61 @@ e_cost.grid(column=7, row=0, sticky=NSEW, padx=(5, 0))
 
 # ANZEIGEN ------------------------------------------------------------------
 # anzeige kwh pro tag
+s_items = Scrollbar(mainFrame)
+s_items.grid(column=1, row=1, sticky=[N, S, W], pady=(15, 0))
+
+lb_items = Listbox(mainFrame, yscrollcommand=s_items.set)
+lb_items.grid(column=0, row=1, sticky=W, pady=(15, 0))
+
+s_items.configure(command=lb_items.yview)
 l_kwh_day = ttk.Label(mainFrame, text="Verbrauch pro Tag:")
-l_kwh_day.grid(column=0, row=1, sticky=W, pady=(10, 0))
+l_kwh_day.grid(column=0, row=2, sticky=W, pady=(10, 0))
 
 l_kwh_day_2 = ttk.Label(mainFrame, text="")
-l_kwh_day_2.grid(column=1, row=1, sticky=W, padx=(4, 0), pady=(10, 0))
+l_kwh_day_2.grid(column=1, row=2, sticky=W, padx=(4, 0), pady=(10, 0))
 # anzeige kwh pro woche
 l_kwh_week = ttk.Label(mainFrame, text="Verbrauch pro Woche:")
-l_kwh_week.grid(column=0, row=2, sticky=W)
+l_kwh_week.grid(column=0, row=3, sticky=W)
 
 l_kwh_week_2 = ttk.Label(mainFrame, text="")
-l_kwh_week_2.grid(column=1, row=2, sticky=W, padx=(4, 0))
+l_kwh_week_2.grid(column=1, row=3, sticky=W, padx=(4, 0))
 # anzeige kwh pro monat
 l_kwh_month = ttk.Label(mainFrame, text="Verbrauch pro Monat:")
-l_kwh_month.grid(column=0, row=3, sticky=W)
+l_kwh_month.grid(column=0, row=4, sticky=W)
 
 l_kwh_month_2 = ttk.Label(mainFrame, text="")
-l_kwh_month_2.grid(column=1, row=3, sticky=W, padx=(4, 0))
+l_kwh_month_2.grid(column=1, row=4, sticky=W, padx=(4, 0))
 # anzeige kwh pro jahr
 l_kwh_year = ttk.Label(mainFrame, text="Verbrauch pro Jahr:")
-l_kwh_year.grid(column=0, row=4, sticky=W)
+l_kwh_year.grid(column=0, row=5, sticky=W)
 
 l_kwh_year_2 = ttk.Label(mainFrame, text="")
-l_kwh_year_2.grid(column=1, row=4, sticky=W, padx=(4, 0))
+l_kwh_year_2.grid(column=1, row=5, sticky=W, padx=(4, 0))
 # KOSTEN
 # anzeige kosten pro tag
 l_cost_day = ttk.Label(mainFrame, text="Kosten pro Tag:")
-l_cost_day.grid(column=0, row=5, sticky=W, pady=(10, 0))
+l_cost_day.grid(column=0, row=6, sticky=W, pady=(10, 0))
 
 l_cost_day_2 = ttk.Label(mainFrame, text="")
-l_cost_day_2.grid(column=1, row=5, sticky=NSEW, padx=(4, 0), pady=(10, 0))
+l_cost_day_2.grid(column=1, row=6, sticky=NSEW, padx=(4, 0), pady=(10, 0))
 # anzeige kosten pro woche
 l_cost_week = ttk.Label(mainFrame, text="Kosten pro Woche:")
-l_cost_week.grid(column=0, row=6, sticky=W)
+l_cost_week.grid(column=0, row=7, sticky=W)
 
 l_cost_week_2 = ttk.Label(mainFrame, text="")
-l_cost_week_2.grid(column=1, row=6, sticky=W, padx=(4, 0))
+l_cost_week_2.grid(column=1, row=7, sticky=W, padx=(4, 0))
 # anzeige kosten pro monat
 l_cost_month = ttk.Label(mainFrame, text="Kosten pro Monat:")
-l_cost_month.grid(column=0, row=7, sticky=W)
+l_cost_month.grid(column=0, row=8, sticky=W)
 
 l_cost_month_2 = ttk.Label(mainFrame, text="")
-l_cost_month_2.grid(column=1, row=7, sticky=W, padx=(4, 0))
+l_cost_month_2.grid(column=1, row=8, sticky=W, padx=(4, 0))
 # anzeige kosten pro jahr
 l_cost_year = ttk.Label(mainFrame, text="Kosten pro Jahr:")
-l_cost_year.grid(column=0, row=8, sticky=W)
+l_cost_year.grid(column=0, row=9, sticky=W)
 
 l_cost_year_2 = ttk.Label(mainFrame, text="")
-l_cost_year_2.grid(column=1, row=8, sticky=W, padx=(4, 0))
+l_cost_year_2.grid(column=1, row=9, sticky=W, padx=(4, 0))
 
 # rechenoperation
 def calculate():
@@ -106,61 +115,12 @@ def calculate():
         hours_day = int(e_hours_day.get())
     days_per_week = int(com_days_per_week.get())
 
-    try:
-        l_kwh_day_2.configure(text=f"{round(watt*hours_day/1000,1)}0 kWh")
-    except:
-        pass
-
-    try:
-        l_kwh_week_2.configure(
-            text=f"{round(watt*hours_day*days_per_week/1000,1)}0 kWh"
-        )
-    except:
-        pass
-
-    try:
-        l_kwh_month_2.configure(
-            text=f"{round(watt*hours_day*days_per_week*4.3/1000,1)}0 kWh"
-        )
-    except:
-        pass
-
-    try:
-        l_kwh_year_2.configure(
-            text=f"{round(watt*hours_day*days_per_week*52.1/1000,1)}0 kWh"
-        )
-    except:
-        pass
-    # KOSTEN
-    try:
-        l_cost_day_2.configure(text=f"{format(watt*hours_day/1000*cost,'.2f')} €")
-    except:
-        pass
-
-    try:
-        l_cost_week_2.configure(
-            text=f"{format(watt*hours_day*days_per_week/1000*cost,'.2f')} €"
-        )
-    except:
-        pass
-
-    try:
-        l_cost_month_2.configure(
-            text=f"{format(watt*hours_day*days_per_week*4.3/1000*cost,'.2f')} €"
-        )
-    except:
-        pass
-
-    try:
-        l_cost_year_2.configure(
-            text=f"{format(watt*hours_day*days_per_week*52.1/1000*cost,'.2f')} €"
-        )
-    except:
-        pass
+    lb_items.insert(99999999, cost)
+    lb_items.grid(column=0, row=1, sticky=W, pady=(15, 0))
 
 
 # knopf ausrechnen
 button_calculate = ttk.Button(mainFrame, text="Berechnen", command=calculate)
-button_calculate.grid(column=0, row=9, sticky=W, pady=(10, 0))
+button_calculate.grid(column=0, row=10, sticky=W, pady=(10, 0))
 
 root.mainloop()
